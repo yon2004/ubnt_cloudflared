830a60bf5e93c4813f2f65805aef51c7efd1664d
yon2004 <yon2004@gmail.com>
2018-04-12 18:03:30 +1000

    Update cloudflared-install.sh

--- cloudflared-install.sh
+++ cloudflared-install.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 set -e
 
 echo "Are you sure you wish to disable the default DNS server and install cloudflared proxy?" 
@@ -18,6 +17,18 @@ commit
 save
 exit
 
+if [-f /etc/init.d/cloudflared-dns]; then
+    echo "found /etc/init.d/cloudflared-dns stopping service"
+	sudo /etc/init.d/cloudflared-dns stop
+	echo "found /etc/init.d/cloudflared-dns removing previous version"
+	rm /etc/init.d/cloudflared-dns
+fi
+
+if [-f /usr/local/bin/cloudflared]; then
+	echo "found /usr/local/bin/cloudflared removing previous version"
+    rm /usr/local/bin/cloudflared
+fi
+
 echo "downloading cloudflared binary"
 sudo curl https://raw.githubusercontent.com/yon2004/ubnt_cloudflared/master/cloudflared --output /usr/local/bin/cloudflared
 echo "downloading cloudflared init.d script"
@@ -27,6 +38,6 @@ sudo chmod +x /usr/local/bin/cloudflared
 echo "setting execute permission on /etc/init.d/cloudflared-dns"
 sudo chmod +x /etc/init.d/cloudflared-dns
 echo "starting cloudflared-dns"
-/etc/init.d/cloudflared-dns start
+sudo /etc/init.d/cloudflared-dns start
 
 echo "cloudflared has been installed"
