644c5ec35da51e605243524fb0c1d4d5ffc3feea
yon2004 <yon2004@gmail.com>
2018-04-12 18:15:53 +1000

    Update cloudflared-install.sh

--- cloudflared-install.sh
+++ cloudflared-install.sh
@@ -1,6 +1,6 @@
 set -e
 
-echo "Are you sure you wish to disable the default DNS server and install cloudflared proxy?" 
+echo "do you wish to disable the default DNS server and install cloudflared proxy?" 
 read -r -p "Are you sure? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
@@ -11,32 +11,31 @@ else
 fi
 
 echo "removing dns server" 
-configure
-delete service dns forwarding
-commit
-save
-exit
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper delete service dns forwarding
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end
 
 if [-f /etc/init.d/cloudflared-dns]; then
     echo "found /etc/init.d/cloudflared-dns stopping service"
-	sudo /etc/init.d/cloudflared-dns stop
-	echo "found /etc/init.d/cloudflared-dns removing previous version"
-	rm /etc/init.d/cloudflared-dns
+    sudo /etc/init.d/cloudflared-dns stop
+    echo "found /etc/init.d/cloudflared-dns removing previous version"
+    sudo rm /etc/init.d/cloudflared-dns
 fi
 
 if [-f /usr/local/bin/cloudflared]; then
-	echo "found /usr/local/bin/cloudflared removing previous version"
-    rm /usr/local/bin/cloudflared
+    echo "found /usr/local/bin/cloudflared removing previous version"
+    sudo rm /usr/local/bin/cloudflared
 fi
 
 echo "downloading cloudflared binary"
-sudo curl https://raw.githubusercontent.com/yon2004/ubnt_cloudflared/master/cloudflared --output /usr/local/bin/cloudflared
+sudo /usr/bin/curlcurl https://raw.githubusercontent.com/yon2004/ubnt_cloudflared/master/cloudflared --output /usr/local/bin/cloudflared
 echo "downloading cloudflared init.d script"
-sudo curl https://raw.githubusercontent.com/yon2004/ubnt_cloudflared/master/cloudflared-dns --output /etc/init.d/cloudflared-dns
+sudo /usr/bin/curlcurl https://raw.githubusercontent.com/yon2004/ubnt_cloudflared/master/cloudflared-dns --output /etc/init.d/cloudflared-dns
 echo "setting execute permission on /usr/local/bin/cloudflared"
-sudo chmod +x /usr/local/bin/cloudflared
+sudo /bin/chmod/chmod +x /usr/local/bin/cloudflared
 echo "setting execute permission on /etc/init.d/cloudflared-dns"
-sudo chmod +x /etc/init.d/cloudflared-dns
+sudo /bin/chmod/chmod +x /etc/init.d/cloudflared-dns
 echo "starting cloudflared-dns"
 sudo /etc/init.d/cloudflared-dns start
 
