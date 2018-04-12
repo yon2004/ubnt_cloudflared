fff8a25d7d3bf1f98249b3bdd027adc1f4313f6d
yon2004 <yon2004@gmail.com>
2018-04-12 19:23:39 +1000

    Create cloudflared-unintall.sh

---
+++ cloudflared-unintall.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+echo "Version 0.1"
+echo "Do you wish to remove cloudflared proxy and changes made to the DNS server?" 
+read -r -p "Are you sure? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+    echo "Starting uninstall" 
+else
+    echo "exit cloudflared-uninstaller"
+	exit 1
+fi
+
+echo "Remove cloudflared files"
+sudo /etc/init.d/cloudflared-dns stop
+sudo rm /etc/init.d/cloudflared-dns
+sudo rm /usr/local/bin/cloudflared
+
+echo "revert configuration changes"
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper delete service dns forwarding options "no-resolv"
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper delete service dns forwarding options "server=127.0.0.1#5053"
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end
+
+echo "uninstalled cloudflared"
