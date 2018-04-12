411b1d162e0061bc01d08ee07d269468f60e645b
yon2004 <yon2004@gmail.com>
2018-04-12 18:23:39 +1000

    Update cloudflared-install.sh

--- cloudflared-install.sh
+++ cloudflared-install.sh
@@ -17,14 +17,14 @@ echo "removing dns server"
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit
 /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end
 
-if [-f /etc/init.d/cloudflared-dns]; then
+if [ -f /etc/init.d/cloudflared-dns]; then
     echo "found /etc/init.d/cloudflared-dns stopping service"
     sudo /etc/init.d/cloudflared-dns stop
     echo "found /etc/init.d/cloudflared-dns removing previous version"
     sudo rm /etc/init.d/cloudflared-dns
 fi
 
-if [-f /usr/local/bin/cloudflared]; then
+if [ -f /usr/local/bin/cloudflared]; then
     echo "found /usr/local/bin/cloudflared removing previous version"
     sudo rm /usr/local/bin/cloudflared
 fi
