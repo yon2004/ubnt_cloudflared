ac0d6604bccc8a7d702c4cd2a5ece725c9cc33e3
yon2004 <yon2004@gmail.com>
2018-04-17 21:15:38 +1000

    Update cloudflared-install.sh

--- cloudflared-install.sh
+++ cloudflared-install.sh
@@ -4,12 +4,11 @@ set -e
 echo "Version 0.2c"
 echo "Do you wish to configure the default DNS server and install cloudflared proxy?" 
 read -r -p "Are you sure? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
-then
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]] then
     echo "Starting configuration" 
 else
     echo "exit cloudflared-installer"
-	exit 1
+    exit 1
 fi
 
 echo "configure dns server" 
@@ -30,6 +29,27 @@ fi
 if [ -f /usr/local/bin/cloudflared ]; then
     echo "found /usr/local/bin/cloudflared removing previous version"
     sudo rm /usr/local/bin/cloudflared
+fi
+
+if [ -f /etc/cloudflared/config.yml ]; then
+    read -r -p "found /etc/cloudflared/config.yml would you like to install the repository maintainers configuration?? [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]] then
+            echo "Replacing your configuration. Moving your configuration to /tmp/cloudflared-config.yml-backup"
+            sudo mv /etc/cloudflared/config.yml /tmp/cloudflared-config.yml-backup
+        else
+            echo "Keeping your configuration."
+	fi
+    sudo rm /usr/local/bin/cloudflared
+fi
+
+if [ ! -d /etc/cloudflared ]; then
+    echo "make folder /etc/cloudflared"
+    sudo mkdir /etc/cloudflared
+fi
+
+if [ ! -f /etc/cloudflared/config.yml ]; then
+    echo "Downloading config.yml"
+    sudo /usr/bin/curl https://raw.githubusercontent.com/yon2004/ubnt_cloudflared/master/config.yml --output /etc/cloudflared/config.yml
 fi
 
 echo "downloading cloudflared binary"
