eb715e5c12739f0dde3b5e7e32cb9ff347a757a8
yon2004 <yon2004@gmail.com>
2018-04-18 23:25:07 +1000

    Update cloudflared-uninstall.sh

--- cloudflared-uninstall.sh
+++ cloudflared-uninstall.sh
@@ -4,20 +4,30 @@ set -e
 echo "Version 0.2"
 echo "Do you wish to remove cloudflared proxy and changes made to the DNS server?" 
 read -r -p "Are you sure? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
-then
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
     echo "Starting uninstall" 
 else
     echo "exit cloudflared-uninstaller"
 	exit 1
 fi
 
-echo "revert configuration changes"
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper delete service dns forwarding options "no-resolv"
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper delete service dns forwarding options "server=127.0.0.1#5053"
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit
-/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end
+read -r -p "Do you want to use Cloudflare DNS servers rather than your ISP? [y/N]" response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+    echo "revert configuration changes and pointing dns to 1.1.1.1"
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper delete service dns forwarding options "server=127.0.0.1#5053"
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set service dns forwarding options "server=1.1.1.1"
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set service dns forwarding options "server=1.0.0.1"
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end
+else
+    echo "revert configuration changes"
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper delete service dns forwarding options "no-resolv"
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper delete service dns forwarding options "server=127.0.0.1#5053"
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit
+    /opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end
+fi
 
 echo "Remove cloudflared files"
 sudo /etc/init.d/cloudflared stop
